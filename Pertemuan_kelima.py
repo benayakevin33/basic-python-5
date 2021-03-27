bac0f2041be102047cd1df142970aab5087ef7ac
benaya333 <49364485+benaya333@users.noreply.github.com>
2021-03-27 14:35:08 +0700

    pertemuan kelima

---
+++ Pertemuan_kelima.py
@@ -0,0 +1,3 @@
+def greeting (nama):
+    print ("Hello" + nama)
+
---
+++ test_module.py
@@ -0,0 +1,19 @@
+# import Pertemuan_kelima 
+
+# Pertemuan_kelima.greeting("Benaya")
+
+##################################### IMPORT FUNGSI MATEMATIKA #############################################
+# import math
+# x = math.pi
+
+# print (x)
+
+# y = math.sqrt (225)
+# print (y)
+
+##################################### IMPORT FUNGSI MATEMATIKA #############################################
+
+import datetime
+
+today = datetime.datetime.now ()
+print (today)
